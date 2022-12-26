3a9719bd5d11c502b7b8cd7681e6c3192c26d388
Khalil Holmes <86483520+cloudstrise@users.noreply.github.com>
2022-12-26 10:37:40 -0600

    Initializing Repository

---
+++ app.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let locationIcon = document.querySelector('.weather-icon');
+    let temperatureSection = document.querySelector('.temperature');
+    let temperatureSpan = document.querySelector('.temperature span');
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            let apiKey = '1bf954ba4fd9d3465c6db8d253ae5042';
+
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;               
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+
+                .then(data => {
+                  console.log(data);
+                  const { name } = data;
+                  const { temp } = data.main;
+                  const { description } = data.weather[0];
+                  const { icon } = data.weather[0];
+
+                  //Set DOM Elements from API
+                  temperatureDegree.textContent = temp;
+                  temperatureDescription.textContent = description;
+                  locationTimezone.textContent = name;
+
+                  //Celcius Formula
+                  let celsius = (temp - 32) * (5 / 9);
+
+                  //Set Icon
+                  locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
+
+                  console.log(temp, description, icon);
+
+                  //Change temperature to / from Celsius/Farenheit
+                  temperatureSection.addEventListener("click", () => {
+                    if (temperatureSpan.textContent == "F") {
+                      temperatureSpan.textContent = "C";
+                      temperatureDegree.textContent = Math.floor(celsius);
+                    } else {
+                      temperatureSpan.textContent = "F";
+                      temperatureDegree.textContent = temp;
+                    }
+                  });
+
+                })
+        });
+    }
+
+
+});
